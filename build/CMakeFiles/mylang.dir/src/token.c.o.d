96db6e34d94e30296918561242fd7cc2c83271a4
Peppy3 <lindfors.freja@gmail.com>
2025-01-20 18:01:52 +0100

    removed build folder

--- build/CMakeFiles/mylang.dir/src/token.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/mylang.dir/src/token.c.o: /home/admin/code/mylang/src/token.c \
-  /usr/lib/llvm-14/lib/clang/14.0.6/include/stddef.h \
-  /usr/lib/llvm-14/lib/clang/14.0.6/include/__stddef_max_align_t.h \
-  /home/admin/code/mylang/src/token.h
